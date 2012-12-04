5a5dc01b14a47b9eb286d166c3391d61937b6da7
wgchoi <wgchoi@cc10541e-3e23-47dc-98c7-cfce100f231d>
2012-12-04 16:44:33 +0000

    eval added2

--- branches/nec-code/matlab/sample_eval_script.m
+++ branches/nec-code/matlab/sample_eval_script.m
@@ -9,12 +9,12 @@ basenum = 0 - 1;
 % datadir = '/home/wgchoi/datasets/necdata/0104/2011_09_26_drive_0104/image_02/dets/';
 % resdir = '/home/wgchoi/codes/ped_tracker_google/trunk/standalone_tracker/results/0104.car.test_w4.car/';
 % annofile = '/home/wgchoi/datasets/necdata/gtbbox/0104_image02.idl';
-datadir = '/home/wgchoi/datasets/necdata/0093/2011_09_26_drive_0093/image_02/dets/';
-resdir = '/home/wgchoi/codes/ped_tracker_google/trunk/standalone_tracker/results/0093.car.org/';
-annofile = '/home/wgchoi/datasets/necdata/gtbbox/0093_image02.idl';
-% datadir = '/home/wgchoi/datasets/necdata/0056/2011_09_26_drive_0056/image_02/dets/';
-% resdir = '/home/wgchoi/codes/ped_tracker_google/trunk/standalone_tracker/results/0056.car.org/';
-% annofile = '/home/wgchoi/datasets/necdata/gtbbox/0056_image02.idl';
+% datadir = '/home/wgchoi/datasets/necdata/0093/2011_09_26_drive_0093/image_02/dets/';
+% resdir = '/home/wgchoi/codes/ped_tracker_google/trunk/standalone_tracker/results/0093.car.org/';
+% annofile = '/home/wgchoi/datasets/necdata/gtbbox/0093_image02.idl';
+datadir = '/z/wgchoi/datasets/necdata/0056/2011_09_26_drive_0056/image_02/dets/';
+resdir = '/z/wgchoi/codes/ped_tracker_google/trunk/standalone_tracker/results/0056.car.org/';
+annofile = '/z/wgchoi/datasets/necdata/gtbbox/0056_image02.idl';
 %%
 % read annotations in idl format 
 anno = readIDL(annofile);
