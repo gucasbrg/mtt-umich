e1ac723f3eb29fb0d1c6cca6da92b0a8c5982a4a
wgchoi <wgchoi@cc10541e-3e23-47dc-98c7-cfce100f231d>
2013-02-12 16:53:21 +0000

    KITTI data

---
+++ branches/changkyu/KITTI/kitti_download.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# 0004
+wget http://www.mrt.kit.edu/geigerweb/cvlibs.net/kitti/raw_data/2011_09_29_drive_0004/2011_09_29_drive_0004.zip
+wget http://www.mrt.kit.edu/geigerweb/cvlibs.net/kitti/raw_data/2011_09_29_drive_0004/2011_09_29_drive_0004_tracklets.zip
+# 0047
+wget http://www.mrt.kit.edu/geigerweb/cvlibs.net/kitti/raw_data/2011_10_03_drive_0047/2011_10_03_drive_0047.zip
+wget http://www.mrt.kit.edu/geigerweb/cvlibs.net/kitti/raw_data/2011_10_03_drive_0047/2011_10_03_drive_0047_tracklets.zip
+# 0056 : few cars / no peds
+wget http://www.mrt.kit.edu/geigerweb/cvlibs.net/kitti/raw_data/2011_09_26_drive_0056/2011_09_26_drive_0056.zip
+wget http://www.mrt.kit.edu/geigerweb/cvlibs.net/kitti/raw_data/2011_09_26_drive_0056/2011_09_26_drive_0056_tracklets.zip
+# 0093 : many parked cars / few peds
+wget http://www.mrt.kit.edu/geigerweb/cvlibs.net/kitti/raw_data/2011_09_26_drive_0093/2011_09_26_drive_0093.zip
+wget http://www.mrt.kit.edu/geigerweb/cvlibs.net/kitti/raw_data/2011_09_26_drive_0093/2011_09_26_drive_0093_tracklets.zip
+# 0104 : many cars / some peds
+wget http://www.mrt.kit.edu/geigerweb/cvlibs.net/kitti/raw_data/2011_09_26_drive_0104/2011_09_26_drive_0104.zip
+wget http://www.mrt.kit.edu/geigerweb/cvlibs.net/kitti/raw_data/2011_09_26_drive_0104/2011_09_26_drive_0104_tracklets.zip
+# 0117 : some parked cars / few peds
+wget http://www.mrt.kit.edu/geigerweb/cvlibs.net/kitti/raw_data/2011_09_26_drive_0117/2011_09_26_drive_0117.zip
+wget http://www.mrt.kit.edu/geigerweb/cvlibs.net/kitti/raw_data/2011_09_26_drive_0117/2011_09_26_drive_0117_tracklets.zip
